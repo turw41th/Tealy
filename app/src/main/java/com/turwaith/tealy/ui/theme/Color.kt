bc8f78489755aa06392468dc37465f2a5278592f
Turwaith <nicolasbaur@turwaith.dev>
2025-03-02 17:34:06 +0100

    Changes some colors

--- app/src/main/java/com/turwaith/tealy/ui/theme/Color.kt
+++ app/src/main/java/com/turwaith/tealy/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val TeaGreen = Color(0xFF27932A)
+val TeaGrey = Color(0xFF326434)
+val TeaOther = Color(0xFF415919)
--- app/src/main/java/com/turwaith/tealy/ui/theme/Theme.kt
+++ app/src/main/java/com/turwaith/tealy/ui/theme/Theme.kt
@@ -11,10 +11,16 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = TeaGreen,
+    secondary = TeaGrey,
+    tertiary = TeaOther
 )
 
 private val LightColorScheme = lightColorScheme(
